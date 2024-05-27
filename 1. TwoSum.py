5c6022c64c37f6c02510071221ee06d6dbb30912
DuaaShareef <75999852+DuaaAlshareef@users.noreply.github.com>
2024-05-27 00:44:40 +0000

    first commit

---
+++ 1. TwoSum.py
@@ -0,0 +1,17 @@
+
+
+
+
+
+# Brute Force solution to the problem, seeing every possible combination of the pair of numbers that sum up to the target
+# solution complexity is O(n^2)
+# arr=list(map(int,input().split()))
+# target = int(input())
+# for i in range(len(arr)-1):
+#     for j in range(1,len(arr)):
+#         if arr[i]+arr[j]==target:
+#             print('answer: ', i , j)
+
+
+
+
