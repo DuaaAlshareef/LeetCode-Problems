f20117c13869314d2ebebdb4b4ec1be937b2fea3
DuaaShareef <75999852+DuaaAlshareef@users.noreply.github.com>
2024-05-27 12:12:30 +0000

    added to the solution

--- 2. BestTimetoBuyandSellStock.py
+++ 2. BestTimetoBuyandSellStock.py
@@ -24,46 +24,47 @@ while it has the same time and space complexity it performs slightly faster.
 Timing both solutions and comparing between the results.
 '''
 # Sample data
-prices = [7, 1, 5, 3, 6, 4] * 10000  # Making the list large to see the performance difference
+# prices = [7, 1, 5, 3, 6, 4] * 10000  # Making the list large to see the performance difference
 
-start = time.time()
-def maxProfit_min_max(prices):
-    minimum_price = prices[0]
-    max_profit = 0
-    for i in range(len(prices)):
-        minimum_price = min(minimum_price, prices[i])
-        current_profit = prices[i] - minimum_price
-        max_profit = max(current_profit, max_profit)
-    return max_profit
+# start = time.time()
+# def maxProfit_min_max(prices):
+#     minimum_price = prices[0]
+#     max_profit = 0
+#     for i in range(len(prices)):
+#         minimum_price = min(minimum_price, prices[i])
+#         current_profit = prices[i] - minimum_price
+#         max_profit = max(current_profit, max_profit)
+#     return max_profit
 
-print("Max Profit (min/max):", maxProfit_min_max(prices))
-end = time.time()
-print("Time (min/max):", end - start)
+# print("Max Profit (min/max):", maxProfit_min_max(prices))
+# end = time.time()
+# print("Time (min/max):", end - start)
 
 
 
 '''
 Solution using two pointers
 '''
-start = time.time()
-def maxProfit_explicit(prices):
-    n=len(prices)
-    l,r = 0,1
-    max_profit = 0
-    mimimum_price = prices[l]
-    while r < n:
-        if prices[l] < mimimum_price:
-            mimimum_price = prices[l]
-        profit = prices[r] - mimimum_price 
-        if profit > max_profit:
-            max_profit=profit
-        r +=1
-        l +=1
-    return max_profit
 
-print("Max Profit (explicit):", maxProfit_explicit(prices))
-end = time.time()
-print("Time (explicit):", end - start)
+# start = time.time()
+# def maxProfit_explicit(prices):
+#     n=len(prices)
+#     l,r = 0,1
+#     max_profit = 0
+#     mimimum_price = prices[l]
+#     while r < n:
+#         if prices[l] < mimimum_price:
+#             mimimum_price = prices[l]
+#         profit = prices[r] - mimimum_price 
+#         if profit > max_profit:
+#             max_profit=profit
+#         r +=1
+#         l +=1
+#     return max_profit
+
+# print("Max Profit (explicit):", maxProfit_explicit(prices))
+# end = time.time()
+# print("Time (explicit):", end - start)
 
 
 # Output:
